942f84d82a92b30f7f60f479d53c2755c8dd1313
HP <HP@DESKTOP-1F9G2T4>
2023-09-07 13:34:30 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_array_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name_value;
+            Console.WriteLine("please enter how many names you want to enter");
+            int len = Convert.ToInt32(Console.ReadLine());
+            name_value = new string[len];
+            for (int j = 0; j< len; j++ )
+            {
+                Console.WriteLine("please enter name");
+                name_value[j]=Console.ReadLine();
+            }
+            for (int i = 0; i < name_value.Length; i++)
+            {
+                Console.WriteLine(name_value[i]);
+            }
+        Console.ReadKey();
+
+
+        }
+    }
+}
